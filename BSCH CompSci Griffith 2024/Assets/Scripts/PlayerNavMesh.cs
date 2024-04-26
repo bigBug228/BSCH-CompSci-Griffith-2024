36c537232ef535cfdbf5ab63c1bcb3b048137ee4
bigBug228 <103580800+bigBug228@users.noreply.github.com>
2024-04-26 01:47:49 +0100

    Task7

--- BSCH CompSci Griffith 2024/Assets/Scripts/PlayerNavMesh.cs
+++ BSCH CompSci Griffith 2024/Assets/Scripts/PlayerNavMesh.cs
@@ -1,25 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerNavMesh : MonoBehaviour
 {
+    public Animator animator;
+    public NavMeshAgent agent;
+
+    public float currentVelocity;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        //get the velocity of the player
+        currentVelocity = agent.velocity.magnitude;
+        //set the speed of the animator to the velocity of the player
+        animator.SetFloat("velocity", currentVelocity);
+
+        //move towards the mouseclick using navmesh
+        if (Input.GetMouseButtonDown(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit))
             {
-                GetComponent<UnityEngine.AI.NavMeshAgent>().destination = hit.point;
+                agent.destination = hit.point;
             }
         }
     }
